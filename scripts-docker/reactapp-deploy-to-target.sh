a1ff900bbd974f12f137197caa00febe2931add3
Louis Benagha <elitesolutionsit@outlook.com>
2023-04-28 20:01:06 -0500

    updates

--- scripts-docker/reactapp-deploy-to-target.sh
+++ scripts-docker/reactapp-deploy-to-target.sh
@@ -5,21 +5,21 @@ DOCKER_USERNAME="${DOCKER_USERNAME:-dockerhub-username}"
 DOCKER_PASSWORD="${DOCKER_PASSWORD:-dockerhub-token}"
 SERVER_USERNAME="ubuntu"
 SERVER_HOST="34.200.228.201"
-datadog_api_key="${DATADOG_API_KEY:-datadog_api_key}"
+DATADOG_API_KEY="${DATADOG_API_KEY:-datadog_api_key}"
 
 # Run as non root user
 sudo usermod -aG docker $USER
 cd $HOME && git clone https://github.com/techstarterepublic-dev/jenkins-course.git
 
-sed "s/\${datadog_api_key}/$datadog_api_key/g" /home/ubuntu/jenkins-course/docker-compose-frontend.yml
-sed "s/\${datadog_api_key}/$datadog_api_key/g" /home/ubuntu/jenkins-course/datadog-sidecar/datadog.yaml
+sed "s/\${datadog_api_key}/$DATADOG_API_KEY/g" /home/ubuntu/jenkins-course/docker-compose-frontend.yml
+sed "s/\${datadog_api_key}/$DATADOG_API_KEY/g" /home/ubuntu/jenkins-course/datadog-sidecar/datadog.yaml
 
 # Check if variables are set
-if [[ -z $DOCKER_USERNAME || -z $DOCKER_PASSWORD || -z $SERVER_USERNAME || -z $SERVER_HOST || -z $datadog_api_key ]]; then
+if [[ -z $DOCKER_USERNAME || -z $DOCKER_PASSWORD || -z $SERVER_USERNAME || -z $SERVER_HOST || -z $DATADOG_API_KEY ]]; then
     echo "Error: One or more required variables are not set."
     exit 1
 else
     # Run docker-compose on the target server
-    cd /home/ubuntu/jenkins-course && docker-compose -f docker-compose-frontend.yml --env-file <(echo "DD_API_KEY=${datadog_api_key}") up -d
+    cd /home/ubuntu/jenkins-course && docker-compose -f docker-compose-frontend.yml --env-file <(echo "DD_API_KEY=${DATADOG_API_KEY}") up -d
     echo "Deployment successful"
 fi
