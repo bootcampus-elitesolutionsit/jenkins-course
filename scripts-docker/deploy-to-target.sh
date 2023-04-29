6262305158b4e9e379145000848ab0697b8b3e13
admin <124517438+arerepadelouisbenagha@users.noreply.github.com>
2023-04-28 19:01:17 -0500

    Update deploy-to-target.sh

--- scripts-docker/deploy-to-target.sh
+++ scripts-docker/deploy-to-target.sh
@@ -14,11 +14,11 @@ sed "s/\${datadog_api_key}/$datadog_api_key/g" /home/ubuntu/jenkins-course/docke
 sed "s/\${datadog_api_key}/$datadog_api_key/g" /home/ubuntu/jenkins-course/datadog-sidecar/datadog.yaml
 
 # Check if variables are set
-if [[ -z $DOCKER_USERNAME || -z $DOCKER_PASSWORD || -z $SERVER_USERNAME || -z $SERVER_HOST ]]; then
+if [[ -z $DOCKER_USERNAME || -z $DOCKER_PASSWORD || -z $SERVER_USERNAME || -z $SERVER_HOST || -z $datadog_api_key ]]; then
     echo "Error: One or more required variables are not set."
     exit 1
 else
     # Run docker-compose on the target server
-    docker-compose --env-file <(echo "DD_API_KEY=${datadog_api_key}") up -d
+    cd /home/ubuntu/jenkins-course && docker-compose --env-file <(echo "DD_API_KEY=${datadog_api_key}") up -d
     echo "Deployment successful"
 fi
